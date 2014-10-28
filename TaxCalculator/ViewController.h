3730eb912ad091c6957fbc0104beb8ee29bf82a4
Johnny Appleseed <engineering@mobilemakers.co>
2014-10-28 18:07:13 -0500

    Initial Commit

---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Johnny Appleseed on 10/28/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaxCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TaxCalculator
+//
+//  Created by Johnny Appleseed on 10/28/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
