7cf2c81bae7564a53b611498fc498eac90accda6
Tom Carmona <thomascarmona@gmail.com>
2015-05-09 15:54:34 -0500

    first commit

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
 
 @end
 
@@ -16,12 +23,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    CGRect frame= self.segmentedControl.frame;
+    [self.segmentedControl setFrame:CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 100)];
+
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onButtonTapped:(id)sender
+{
+    NSString *enteredText = self.textField.text;
+    double enteredValue = enteredText.intValue;
+
+    if (self.segmentedControl.selectedSegmentIndex == 0)
+    {
+        double result = enteredValue * self.caTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    else if (self.segmentedControl.selectedSegmentIndex == 1)
+    {
+        double result = enteredValue * self.chiTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    else
+    {
+        double result = enteredValue * self.nyTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
 }
+
 
 @end
